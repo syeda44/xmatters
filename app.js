8f3a6e9490730a709b08ef3524708ed3d086508c
kapgarxmatters <kapgar@xmatters.com>
2018-04-21 00:47:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
+//var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
 bot.set('storage', tableStorage);
 
 bot.dialog('/', [
