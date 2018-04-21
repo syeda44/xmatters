405bd5b041b0a838521dd86abcc5bc61caaefd03
kapgarxmatters <kapgar@xmatters.com>
2018-04-21 01:08:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -39,14 +39,8 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 
 var bot = new builder.UniversalBot(connector, function (session) {
    var text = session.message.text;
-   var command = text.split(" ")[0];
-   var extras = text.split(command+" ")[1];
-   switch(command){
-      case "help":
-         session.send("You said" + text); 
-         break;
-   }
-    }).set('storage', inMemoryStorage); // Register in memory storage;
+   session.send("You said" + text); 
+})
 
 
         
