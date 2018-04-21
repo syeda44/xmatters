6b50c2174fb45e88342a493175ac67bdac06996c
kapgarxmatters <kapgar@xmatters.com>
2018-04-20 22:54:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-'use strict';
 
-var express = require('express');
-var app = express();
-
-// Adding tabs to our app. This will setup routes to various views
-var tabs = require('./tabs');
-tabs.setup(app);
-
-// Adding a bot to our app
-var bot = require('./bot');
-bot.setup(app);
-
-// Adding a messaging extension to our app
-var messagingExtension = require('./messaging-extension');
-messagingExtension.setup();
-
-// Start our nodejs app
-app.listen(process.env.PORT || 8080, function() {
-    console.log('App started listening on port 8080');
-});
 
 /*-----------------------------------------------------------------------------
 A simple echo bot for the Microsoft Bot Framework. 
