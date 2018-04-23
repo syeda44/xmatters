474d478d479ba998b8365ef4b8fb1b90be1d16ed
kapgarxmatters <kapgar@xmatters.com>
2018-04-23 10:20:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,26 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 bot.set('storage', tableStorage);
 
+bot.dialog('Hello', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                    " you've been programming for " + session.userData.coding + 
+                    " years and use " + session.userData.language + ".");
+    }
+]).triggerAction({ matches: /^Hello$/i });
+
 bot.dialog('help', [
     function (session) {
         builder.Prompts.text(session, "Hello... What's your name?");
