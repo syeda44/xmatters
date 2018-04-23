6d0d663c931a9fdaf445bca1cecfe3baaacda6a3
kapgarxmatters <kapgar@xmatters.com>
2018-04-23 11:24:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -75,22 +75,13 @@ bot.dialog('Hello', [
 
 bot.dialog('help', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
+       var text = session.message.text;
+       var command = text.split(" ")[0];
+       var extras = text.split(command+" ")[1];
+       //builder.Prompts.text(session, "Hello... What's your name?");
+       help(extras,session);
     }
+    
 ]).triggerAction({ matches: /(Help)\s(.*).*/i });
 
 
